134874b8fb241c73431c26df7f8e513f08858ab9
aqiyzx <78235078+aqiyzx@users.noreply.github.com>
2021-11-03 20:21:58 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,33 @@
+BITRATE="2500k" # Bitrate of the output video
+FPS="30" # FPS video output
+QUAL="medium" # FFMPEG quality preset
+YOUTUBE_URL="rtmp://x.rtmp.youtube.com/live2" # Youtube RTMP base URL
+IMAGE="C:\Users\Aqil\Pictures\670838.jpg" #Picture
+SOURCE="https://open.spotify.com/playlist/6kjG3b4oH9B3w9ptdcnZxN" # Radio Station
+KEY="rtb2-3kde-30af-jyre-463g" # Stream name/key
+SIZE="1280×720"
+FRAMERATE="24"
+
+    ffmpeg -re -loop 1 \
+    	-framerate "$FRAMERATE" \
+    	-i "$IMAGE" \
+    	-i "$SOURCE" \
+    	-c:a aac \
+    	-s "$SIZE" \
+    	-ab 128k \
+    	-b:v "$BITRATE" \
+    	-threads 6 \
+    	-qscale 3 \
+    	-preset veryfast \
+    	-vcodec libx264 \
+    	-pix_fmt yuv420p \
+    	-maxrate 2048k \
+    	-bufsize 2048k \
+    	-framerate 30 \
+    	-g 2 \
+    	-strict experimental \
+    	-f flv \
+    	"$YOUTUBE_URL/$KEY"
+#
+#  Created by Roman on 04/22/2019.
+#  Copyright © 2019 akhromieiev.com. All rights reserved.
