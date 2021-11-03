d9aff32dc02e5aea659f87d1f00fe146278fb16e
aqiyzx <78235078+aqiyzx@users.noreply.github.com>
2021-11-04 04:40:21 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
-BITRATE="2500k" # Bitrate of the output video
+BITRATE="1700k" # Bitrate of the output video
 FPS="30" # FPS video output
 QUAL="medium" # FFMPEG quality preset
 YOUTUBE_URL="rtmp://x.rtmp.youtube.com/live2" # Youtube RTMP base URL
 IMAGE="https://images.hdqwalls.com/download/hatsune-miku-anime-art-5k-d5-1280x720.jpg" #Picture
 SOURCE="https://streamingv2.shoutcast.com/japanimradio-osaka" # Radio Station
 KEY="rtb2-3kde-30af-jyre-463g" # Stream name/key
-SIZE="640x480"
+SIZE="720x480"
 FRAMERATE="1"
 
     ffmpeg -re -loop 1 \
