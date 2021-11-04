d9e0483f4b7c26a64b2d5f274f057dbb11661343
aqiyzx <78235078+aqiyzx@users.noreply.github.com>
2021-11-04 23:50:01 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -19,7 +19,7 @@ FRAMERATE="2"
     	-threads 6 \
     	-qscale 3 \
     	-preset veryfast \
-    	-vcodec libx265 \
+    	-vcodec libx264 \
     	-pix_fmt yuv420p \
     	-maxrate 1800k \
     	-bufsize 3300k \
