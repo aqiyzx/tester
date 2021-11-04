a9dafb9330bf1798adffa95c9ef8bdfe10cc04a9
aqiyzx <78235078+aqiyzx@users.noreply.github.com>
2021-11-04 20:28:35 +0700

    Update main1.sh

--- main1.sh
+++ main1.sh
@@ -21,8 +21,8 @@ FRAMERATE="2"
     	-preset veryfast \
     	-vcodec libx264 \
     	-pix_fmt yuv420p \
-    	-maxrate 1289k \
-    	-bufsize 12000k \
+    	-maxrate 2500k \
+    	-bufsize 27000k \
     	-framerate 30 \
     	-g 2 \
     	-strict experimental \
