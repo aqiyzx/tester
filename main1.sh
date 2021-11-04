427fd7514dbd7e1144a4e1569f79e9733687c50e
aqiyzx <78235078+aqiyzx@users.noreply.github.com>
2021-11-04 09:32:54 +0700

    Update main1.sh

--- main1.sh
+++ main1.sh
@@ -1,7 +1,7 @@
 BITRATE="3127k" # Bitrate of the output video
 FPS="30" # FPS video output
 QUAL="medium" # FFMPEG quality preset
-YOUTUBE_URL="rtmp://x.rtmp.youtube.com/live2" # Youtube RTMP base URL
+YOUTUBE_URL="rtmp://y.rtmp.youtube.com/live2?backup=1" # Youtube RTMP base URL
 IMAGE="https://images.hdqwalls.com/download/hartsune-miku-vocaloid-anime-girl-5k-1z-1280x720.jpg" #Picture
 SOURCE="http://curiosity.shoutca.st:8019/stream" # Radio Station
 KEY="rtb2-3kde-30af-jyre-463g" # Stream name/key
@@ -27,10 +27,9 @@ FRAMERATE="1"
     	-g 2 \
     	-strict experimental \
         -fflags nobuffer \
-    	-f flv rtmp://y.rtmp.youtube.com/live2?backup=1 \
+    	-f flv \
     	"$YOUTUBE_URL/$KEY"
-     ffplay -f lavfi
-     net.core.rmem_max=26214400
+
    
 #
 #  Created by Roman on 04/22/2019.
