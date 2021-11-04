e538300bba55b46cf2e8784423bcc34cbd797e94
aqiyzx <78235078+aqiyzx@users.noreply.github.com>
2021-11-05 01:30:10 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -21,8 +21,8 @@ FRAMERATE="2"
     	-preset ultrafast \
     	-vcodec libx264 \
     	-pix_fmt yuv420p \
-    	-maxrate 1800k \
-    	-bufsize 3300k \
+    	-maxrate 2500k \
+    	-bufsize 2500k \
     	-framerate 30 \
     	-g 2 \
     	-strict experimental \
