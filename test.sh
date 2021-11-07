59304d095d1e0aa541659a2c64d42028860e5162
aqiyzx <78235078+aqiyzx@users.noreply.github.com>
2021-11-08 01:22:16 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,6 +8,7 @@ YOUTUBE_KEY="rtb2-3kde-30af-jyre-463g"
 IMAGE_SOURCE="/root/media/hatsune-miku-anime-art-5k-d5-1280x720.jpg"
 AUDIO_SOURCE="/root/media/music/list.txt"
 AUDIO_ENCODER="aac"
+SIZE="1280X720"
 
 ffmpeg \
  -stream_loop -1 \
@@ -15,7 +16,7 @@ ffmpeg \
  -i "$VIDEO_SOURCE" \
  -thread_queue_size 512 \
  -i "$AUDIO_SOURCE" \
- -c:v libx264 -preset $QUAL -r $FPS -g $(($FPS *2)) -b:v $VBR \
+ -c:v libx264 -s "$SIZE" -preset $QUAL -r $FPS -g $(($FPS *2)) -b:v $VBR \
  -c:a $AUDIO_ENCODER -threads 8 -qscale 3 -ar 44100 -b:a 128k -bufsize 5000k -pix_fmt yuv420p \
  -fflags +shortest -max_interleave_delta 50000 \
  -f flv $YOUTUBE_URL/$YOUTUBE_KEY
