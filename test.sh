2c4d6dc8d6389fb1d45ec0b7aad6aaa69d9e0a39
aqiyzx <78235078+aqiyzx@users.noreply.github.com>
2021-11-08 00:23:06 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,42 +1,36 @@
-BITRATE="2500k" # Bitrate of the output video                                  
-FPS="30" # FPS video output
-QUAL="medium" # FFMPEG quality preset
-YOUTUBE_URL="rtmp://x.rtmp.youtube.com/live2" # Youtube RTMP base URL
-IMAGE="/root/media/hatsune-miku-anime-art-5k-d5-1280x720.jpg" #Picture
-FOLDER="/root/media/music"
+#! /bin/bash
+#
+# Diffusion youtube avec ffmpeg
+
+# Configurer youtube avec une résolution 720p. La vidéo n'est pas scalée.
+
+VBR="2500k"                                                             # Bitrate de la vidéo en sortie
+FPS="30"                                                                # FPS de la vidéo en sortie
+QUAL="medium"                                                           # Preset de qualité FFMPEG
+YOUTUBE_URL="rtmp://x.rtmp.youtube.com/live2"                           # URL de base RTMP youtube
+IMAGE="/root/media/hatsune-miku-anime-art-5k-d5-1280x720.jpg"           #Picture
+
+FOLDER="/root/media/music"                                         # Dossier source
+KEY="rtb2-3kde-30af-jyre-463g"                                     # Clé à récupérer sur l'event youtube
+
+SOURCE=""
 n=0
 filter=""
-KEY="rtb2-3kde-30af-jyre-463g" # Stream name/key
-SIZE="1280x720" # Video Size (1280x720, 720x480, 680x510)
-FRAMERATE="2"
 
+for f in $FOLDER/*.mp3
+do
+  SOURCE="$SOURCE -i $f"
+  filter="$filter [$n:v:0] [$n:a:0]"
+  ((n++))
+done
 
-    ffmpeg -re -loop 1 \
-      -framerate "$FRAMERATE" \
-      -i "$IMAGE" \
-      -i "$FOLDER"/*.mp3 -filter_complex -map 0:v -map "[audio]" -deinterlace \
-      -c:a aac \
-    	-s "$SIZE" \
-      -acodec libmp3lame \
-      -ar 44100 \
-    	-ab 128k \
-      -b:a 712000 \
-      -b:v "$BITRATE" \
-      -threads 8 \
-      -qscale 3 \
-      -preset normal \
-      -vcodec libx264 \
-      -pix_fmt yuv420p \
-      -maxrate 2500k \
-      -bufsize 5000k \
-      -framerate 30 \
-      -g 2 \
-      -strict experimental \
-      -f flv \
-      "$YOUTUBE_URL/$KEY"
- 
- 
- #
-#  Created by Roman on 04/22/2019.
-#  Modified by Aqiyzx on 05/11/2021
-#  Copyright © 2021 akhromieiev.com. All rights reserved.
+filter="$filter concat=n=$n:v=1:a=1 [v] [a]"
+
+echo "ffmpeg $SOURCE -filter_complex '$filter'"
+
+ffmpeg \
+    $SOURCE -filter_complex "$filter" \
+-map "[v]" -map "[a]" -deinterlace \
+    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS)) -b:v $VBR \
+    -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
+    -f flv "$YOUTUBE_URL/$KEY"
