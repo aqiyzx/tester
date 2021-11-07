518076b14cdfbb0f01179a060a92b138dfcdde5f
aqiyzx <78235078+aqiyzx@users.noreply.github.com>
2021-11-07 21:57:38 +0700

    Update main1.sh

--- main1.sh
+++ main1.sh
@@ -30,8 +30,7 @@ FRAMERATE="2"
     	-strict experimental \
     	-f flv \
     	"$YOUTUBE_URL/$KEY"
-     ffplay -fflags nobuffer -flags low_delay -framedrop \
-        -strict experimental -rtsp_transport tcp rtsp://2a01:4f8:13a:19e5:1041:fc65:e2c7:0001:22
+
    
 #
 #  Created by Roman on 04/22/2019.
